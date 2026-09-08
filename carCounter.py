fb849d701a89cb518d6969b8ffcd891197812ed9
GIW065 <isaac.wegner@outlook.com>
2026-09-08 14:49:14 -0600

    removed unecessary outer loop and unnecessary variables

--- carCounter.py
+++ carCounter.py
@@ -7,43 +7,31 @@ s_colour = df['Colour'].drop_duplicates() # Creates a pandas series with only th
 s_model = df['Model'].drop_duplicates() # Creates a pandas series with only the unique models from df
 
 num_colours = 0
-# colour_list = []
 colour_dict = {}
 
 num_models = 0
-# model_list = []
 model_dict = {}
 
 for i in s_colour: # iterate over series
-    #colour_list.append(i) # add index i to the list, final result will be a list with only the 
     colour_dict.update({num_colours : s_colour.iloc[num_colours]}) # add colour as value to dictionary paried with key = iteration #
     num_colours += 1 # increment num_colours by one
 for i in s_model:
-    #model_list.append(i)
     model_dict.update({num_models : s_model.iloc[num_models]})
     num_models += 1
 
 cube = [[0]*num_models]*num_colours # instantiate a 2d array (cube) with num_models columns and num_colours rows
 
-for key in colour_dict:
-    """ temp = df[df['Colour'] == colour_dict.get(key)]
-    temp2 = df[df['Model'] == model_dict.get(key)]
-    print(f'The number of {colour_dict.get(key)} cars is {temp.shape[0]}')
-    print(f'The number of {model_dict.get(key)}\'s: {temp2.shape[0]}') """
-
-    for i in range(df.shape[0]):
-        temp_colour = df.iloc[i, 0]
-        temp_model = df.iloc[i, 1]
-
-        if value in colour_dict.items() == temp_colour:
-            print(key)
-        """ for key, value in colour_dict.items():
-            if temp_colour == value:
-                temp_row = key 
-        for key, value in model_dict.items():
-            if temp_model == value:
-                temp_column = key   """
-        #cube[temp_row][temp_column] += 1
+for i in range(df.shape[0]):
+    temp_colour = df.iloc[i, 0]
+    temp_model = df.iloc[i, 1]
+    for key, value in colour_dict.items():
+        if temp_colour == value:
+            temp_row = key 
+    for key, value in model_dict.items():
+        if temp_model == value:
+            temp_column = key  
+    cube[temp_row][temp_column] += 1
+        
 
 for row in cube:
     print(row)
